d2761394a47365ca0d400b0a48edcd06d04448ce
thoson <thoson.it@gmail.com>
2022-12-23 15:21:30 +0700

    add open cv

---
+++ example/ios/Runner/OpenCVWrapper.h
@@ -0,0 +1,17 @@
+//
+//  OpenCVWrapper.h
+//  Runner
+//
+//  Created by Lê Thọ Sơn on 23/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+#include <opencv2/opencv.hpp>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OpenCVWrapper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ example/ios/Runner/OpenCVWrapper.m
@@ -0,0 +1,13 @@
+//
+//  OpenCVWrapper.m
+//  Runner
+//
+//  Created by Lê Thọ Sơn on 23/12/2022.
+//
+
+#import "OpenCVWrapper.h"
+#include <opencv2/opencv.hpp>
+
+@implementation OpenCVWrapper
+
+@end
